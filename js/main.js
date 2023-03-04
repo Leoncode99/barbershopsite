5bd2cb5ccd51c658740c80dac41f6aa2e3bb792f
Leoncode99 <126984471+Leoncode99@users.noreply.github.com>
2023-03-04 22:51:22 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// header nav
+function toggleMenu() {
+    const toggleMenu = document.querySelector(".toggleMenu");
+    const navigation = document.querySelector(".navigation");
+    toggleMenu.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
